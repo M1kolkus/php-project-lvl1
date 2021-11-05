28de58aea9e269c56c09c82a9c1ca54a3d019615
Torchyk Mikolai <M1kolikus@yandex.by>
2021-11-05 23:47:32 +0300

    step 8

---
+++ src/Progression.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Src\Progression;
+
+use function cli\line;
+use function cli\prompt;
+
+function progression()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('What number is missing in the progression?');
+    for ($i = 0; $i < 3; $i++) {
+        $firstNumber = rand(0, 100);
+        $progressionLength = rand(5, 10);
+        $intervalProgression = rand(0, 10);
+        $emptyValue = rand(1, $progressionLength);
+        $setProgression = [];
+        $setProgression[0] = $firstNumber;
+        $result = '';
+        for ($j = 1; $j < $progressionLength; $j++) {
+            $setProgression[] = $firstNumber + $intervalProgression;
+            $firstNumber = $firstNumber + $intervalProgression;
+        }
+        $rightAnswer = $setProgression[$emptyValue];
+        $setProgression[$emptyValue] = '..';
+        for ($k = 0; $k < count($setProgression); $k++) {
+            $result = "$result $setProgression[$k]";
+        }
+        line("Question: {$result}");
+        $answer = prompt('Your answer');
+        if ($rightAnswer == $answer) {
+            line('Correct!');
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{ $rightAnswer}'.");
+            line("Let's try again, {$name}!");
+            return false;
+        }
+    }
+    line("Congratulations, {$name}");
+}
