79a6bda164bf86a639e674bd945e08414047396d
Torchyk Mikolai <M1kolikus@yandex.by>
2021-11-11 21:23:27 +0300

    step 9/3

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -1,34 +1,34 @@
 <?php
 
-namespace Src\Even;
+namespace Src\Games\Even;
 
-use function cli\line;
-use function cli\prompt;
+use function Src\GeneralLogic\printWelcome;
+use function Src\GeneralLogic\question;
+use function Src\GeneralLogic\rightAnswer;
+use function Src\GeneralLogic\wrongAnswer;
+use function Src\GeneralLogic\end;
 
 function even()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $task = 'Answer "yes" if the number is even, otherwise answer "no".';
+    $name = printWelcome($task);
     for ($i = 0; $i < 3; $i++) {
         $randomNumber = rand(0, 100);
-        line("Question: {$randomNumber}");
-        $answer = prompt('Your answer');
-        if ($randomNumber % 2 == 0 && $answer === 'yes') {
-            line('Correct!');
-        } elseif ($randomNumber % 2 != 0 && $answer === 'no') {
-            line('Correct!');
+        $question = "Question: {$randomNumber}";
+        $answer = question($question);
+        if ($randomNumber % 2 == 0 && $answer == 'yes') {
+            rightAnswer();
+        } elseif ($randomNumber % 2 != 0 && $answer == 'no') {
+            rightAnswer();
         } else {
             if ($randomNumber % 2 == 0) {
                 $rightAnswer = 'yes';
             } elseif ($randomNumber % 2 != 0) {
                 $rightAnswer = 'no';
             }
-            line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
-            line("Let's try again, {$name}!");
+            wrongAnswer($answer, $rightAnswer, $name);
             return false;
         }
     }
-    line("Congratulations, {$name}");
+    end($name);;
 }
